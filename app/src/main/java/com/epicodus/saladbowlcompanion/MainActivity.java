e8fd03f0daf67ed1ef2b9b715d0acb28f922276f
Luca Quatela and Nadiya Yeroshkina <nadiya@>
2016-07-25 13:50:22 -0700

    add intents to setUp, rules and about activities

--- app/src/main/java/com/epicodus/saladbowlcompanion/MainActivity.java
+++ app/src/main/java/com/epicodus/saladbowlcompanion/MainActivity.java
@@ -1,13 +1,47 @@
 package com.epicodus.saladbowlcompanion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.playButton) Button mPlayButton;
+    @Bind(R.id.rulesButton) Button mRulesButton;
+    @Bind(R.id.aboutTextView) TextView mAboutTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mPlayButton.setOnClickListener(this);
+        mRulesButton.setOnClickListener(this);
+        mAboutTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mPlayButton) {
+            Intent intent = new Intent (MainActivity.this, SetUpActivity.class);
+            startActivity(intent);
+        }
+
+        if (view == mRulesButton) {
+            Intent intent = new Intent (MainActivity.this, RulesActivity.class);
+            startActivity(intent);
+        }
+
+        if (view == mAboutTextView) {
+            Intent intent = new Intent (MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }
+
     }
 }

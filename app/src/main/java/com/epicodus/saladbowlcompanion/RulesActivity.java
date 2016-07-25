14b4896abb5f5a3d5ed4ea6ae8fc83fb2fc434fb
Adam Craig and Ashley Maceli <adam+ashley@>
2016-07-25 11:51:55 -0700

    Add RulesActivity

---
+++ app/src/main/java/com/epicodus/saladbowlcompanion/RulesActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.saladbowlcompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}

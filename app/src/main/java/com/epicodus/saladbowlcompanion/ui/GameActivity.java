ac928c44ad754fe7ba24dbf64d8bab2a9d92104c
Adam Craig and Ashley Maceli <adam+ashley@>
2016-07-26 11:07:40 -0700

    Implement multiple API calls for animals and moods, move API functionality to SetUpActivity

--- app/src/main/java/com/epicodus/saladbowlcompanion/ui/GameActivity.java
+++ app/src/main/java/com/epicodus/saladbowlcompanion/ui/GameActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.saladbowlcompanion.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.epicodus.saladbowlcompanion.R;
 import com.epicodus.saladbowlcompanion.services.WordService;
@@ -15,42 +16,10 @@ import okhttp3.Response;
 
 public class GameActivity extends AppCompatActivity {
 
-    public ArrayList<String> mAnimals = new ArrayList<>();
-
-    //TODO: SET BUTTERKNIFE BINDS FOR GAME WORDS
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        getAnimalWords();
-    }
-
-    private void getAnimalWords() {
-        final WordService wordService = new WordService();
-        wordService.getAnimalWords(new Callback() {
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) {
-                mAnimals = wordService.processAnimalResults(response);
-
-                GameActivity.this.runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-
-                    }
-                });
-
-            }
-
-
-        });
     }
 }

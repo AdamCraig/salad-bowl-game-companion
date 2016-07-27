3475796164d2e96a965a0bbe66c857574cc2eadc
Ashley Maceli and Nadiya Yeroshkina <nadiya@>
2016-07-27 11:13:04 -0700

    update colors, add styling and image to MainActivity

--- app/src/main/java/com/epicodus/saladbowlcompanion/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/saladbowlcompanion/ui/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         mPlayButton.setOnClickListener(this);
         mRulesButton.setOnClickListener(this);
         mAboutTextView.setOnClickListener(this);

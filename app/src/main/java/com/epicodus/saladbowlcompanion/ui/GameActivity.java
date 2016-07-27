3a18fae169149415a85e98208ff8614b1c2f7c7e
Adam Craig and Luca Quatela <adam+luca@>
2016-07-27 15:32:43 -0700

    Add currentWordList as a clone of MasterWordList, begin passing extras into TeamTransitionActivity intent on clock expiration

--- app/src/main/java/com/epicodus/saladbowlcompanion/ui/GameActivity.java
+++ app/src/main/java/com/epicodus/saladbowlcompanion/ui/GameActivity.java
@@ -23,11 +23,13 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     public Random randomNumberGenerator = new Random();
     public ArrayList<String> masterWordList = new ArrayList<String>();
+    public ArrayList<String> currentWordList = new ArrayList<String>();
     public ArrayList<Team> teamArray = new ArrayList<>();
     public int randomNumber = 0;
     public int currentRoundNumber;
     public int currentTeam;
     public long timeLeft;
+    public boolean roundOver = false;
 
     @Bind(R.id.timerTextView) TextView mTimerTextView;
     @Bind(R.id.teamNameTextView) TextView mTeamNameTextView;
@@ -45,27 +47,34 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         mGuessButton.setOnClickListener(this);
         mPassButton.setOnClickListener(this);
         masterWordList = getIntent().getStringArrayListExtra("gameWordList");
+        currentWordList = (ArrayList<String>) masterWordList.clone();
         currentTeam = getIntent().getIntExtra("currentTeam", 0);
         currentRoundNumber = getIntent().getIntExtra("currentRoundNumber", 1);
         teamArray = Parcels.unwrap(getIntent().getParcelableExtra("teamArray"));
 
         countDownTimer.start();
         mRoundTextView.setText("Round " + currentRoundNumber + "");
-        mWordTextView.setText(masterWordList.get(randomNumber));
+        mWordTextView.setText(currentWordList.get(randomNumber));
     }
 
 
-    CountDownTimer countDownTimer = new CountDownTimer(60000, 1000) {
+    CountDownTimer countDownTimer = new CountDownTimer(6000, 1000) {
         // https://developer.android.com/reference/android/os/CountDownTimer.html
 
         public void onTick(long millisUntilFinished) {
-            mTimerTextView.setText("seconds remaining " + millisUntilFinished / 1000);
+            mTimerTextView.setText("Time Remaining: " + millisUntilFinished / 1000);
             timeLeft = millisUntilFinished;
         }
 
         public void onFinish() {
-            mTimerTextView.setText("Done!");
+            mTimerTextView.setText("Time!");
             Intent intent = new Intent (GameActivity.this, TeamTransitionActivity.class);
+            intent.putExtra("masterWordList", masterWordList);
+            intent.putExtra("currentWordList", currentWordList);
+            intent.putExtra("currentTeam", currentTeam);
+            intent.putExtra("currentRoundNumber", currentRoundNumber);
+            intent.putExtra("teamArray", teamArray);
+            intent.putExtra("roundOver", roundOver);
             startActivity(intent);
         }
     };
@@ -75,18 +84,23 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
         if (view == mGuessButton) {
-            masterWordList.remove(randomNumber);
-            teamArray.get(currentTeam).incrementScore(currentRoundNumber);
-            Log.v("team score", teamArray.get(currentTeam).getRound1Score() + "");
+            currentWordList.remove(randomNumber);
+            Log.v("master list", masterWordList.size() + "");
+            Log.v("current list", currentWordList.size() + "");
 
-            if (masterWordList.isEmpty()) {
+            teamArray.get(currentTeam).incrementScore(currentRoundNumber);
+
+
+            if (currentWordList.isEmpty()) {
+                roundOver = true;
                 // Transition to new round, pass turn to next team
+                // Pass "round over" boolean to TeamTransition?
                 // Pass timeLeft to next activity to save it and perhaps add it to that team's next turn?
             }
         }
 
-        randomNumber = randomNumberGenerator.nextInt(masterWordList.size());
-        mWordTextView.setText(masterWordList.get(randomNumber));
+        randomNumber = randomNumberGenerator.nextInt(currentWordList.size());
+        mWordTextView.setText(currentWordList.get(randomNumber));
     }
 
 

6cfe3b0757161c36f0860889569712aa8b087a6b
Ashley Maceli and Nadiya Yeroshkina <ashley@>
2016-07-27 14:33:31 -0700

    update styling on RulesActivity, add font and material icons

--- app/src/main/java/com/epicodus/saladbowlcompanion/ui/RulesActivity.java
+++ app/src/main/java/com/epicodus/saladbowlcompanion/ui/RulesActivity.java
@@ -1,15 +1,27 @@
 package com.epicodus.saladbowlcompanion.ui;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epicodus.saladbowlcompanion.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class RulesActivity extends AppCompatActivity {
+
+    @Bind(R.id.rulesTitleTextView) TextView mRulesTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+        ButterKnife.bind(this);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/KaushanScript-Regular.otf");
+        mRulesTitleTextView.setTypeface(font);
+
     }
 }

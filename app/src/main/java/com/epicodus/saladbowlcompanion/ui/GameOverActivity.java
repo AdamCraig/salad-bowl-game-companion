66f2aac09e244e7d7595f93b45ad315834d37ef4
Ashley Maceli and Nadiya Yeroshkina <nadiya@>
2016-07-28 14:28:07 -0700

    add GameOverAvtivity with styling in the xml file

---
+++ app/src/main/java/com/epicodus/saladbowlcompanion/ui/GameOverActivity.java
@@ -0,0 +1,16 @@
+package com.epicodus.saladbowlcompanion.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.saladbowlcompanion.R;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+    }
+}

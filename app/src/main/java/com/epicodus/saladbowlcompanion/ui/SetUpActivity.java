c298d9a7daa21e6f4502810c8d01a5e43fb00bf0
Ashley Maceli and Nadiya Yeroshkina <nadiya@>
2016-07-28 10:17:07 -0700

    change editText to buttons for choosing number of teams

--- app/src/main/java/com/epicodus/saladbowlcompanion/ui/SetUpActivity.java
+++ app/src/main/java/com/epicodus/saladbowlcompanion/ui/SetUpActivity.java
@@ -23,7 +23,11 @@ import okhttp3.Response;
 public class SetUpActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.quickModeButton) Button mQuickModeButton;
     @Bind(R.id.creativeModeButton) Button mCreativeModeButton;
-    @Bind(R.id.teamEditText) EditText mTeamEditText;
+    @Bind(R.id.twoTeamButton) Button mTwoTeamButton;
+    @Bind(R.id.threeTeamButton) Button mThreeTeamButton;
+    @Bind(R.id.fourTeamButton) Button mFourTeamButton;
+    @Bind(R.id.fiveTeamButton) Button mFiveTeamButton;
+    @Bind(R.id.sixTeamButton) Button mSixTeamButton;
 
     public ArrayList<String> mAnimals = new ArrayList<>();
     public ArrayList<String> mMoods = new ArrayList<>();
@@ -34,7 +38,7 @@ public class SetUpActivity extends AppCompatActivity implements View.OnClickList
 
     //TODO: SET BUTTERKNIFE BINDS FOR GAME WORDS
 
-    int numberOfTeamsInt = 2;
+    int numberOfTeamsInt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,16 +53,22 @@ public class SetUpActivity extends AppCompatActivity implements View.OnClickList
 
         mQuickModeButton.setOnClickListener(this);
         mCreativeModeButton.setOnClickListener(this);
+        mTwoTeamButton.setOnClickListener(this);
+        mThreeTeamButton.setOnClickListener(this);
+        mFourTeamButton.setOnClickListener(this);
+        mFiveTeamButton.setOnClickListener(this);
+        mSixTeamButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        String numberOfTeamsString = mTeamEditText.getText().toString();
-        numberOfTeamsInt = Integer.parseInt(numberOfTeamsString);
-
-        if (numberOfTeamsString.equals("") || Integer.parseInt(numberOfTeamsString) > 6 || Integer.parseInt(numberOfTeamsString) < 2) {
-            mTeamEditText.setError("Please enter a valid number of teams.");
-        } else if (view == mQuickModeButton) {
+//        String numberOfTeamsString = mTeamEditText.getText().toString();
+//        numberOfTeamsInt = Integer.parseInt(numberOfTeamsString);
+//
+//        if (numberOfTeamsString.equals("") || Integer.parseInt(numberOfTeamsString) > 6 || Integer.parseInt(numberOfTeamsString) < 2) {
+//            mTeamEditText.setError("Please enter a valid number of teams.");
+//        }
+        if (view == mQuickModeButton) {
             Intent intent = new Intent(SetUpActivity.this, QuickModeActivity.class);
             intent.putExtra("numberOfTeams", numberOfTeamsInt);
             intent.putExtra("masterWordList", mMasterWordList);
@@ -67,6 +77,16 @@ public class SetUpActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(SetUpActivity.this, CreativeModeActivity.class);
             intent.putExtra("numberOfTeams", numberOfTeamsInt);
             startActivity(intent);
+        } else if (view == mTwoTeamButton) {
+            numberOfTeamsInt = 2;
+        } else if (view == mThreeTeamButton) {
+            numberOfTeamsInt = 3;
+        } else if (view == mFourTeamButton) {
+            numberOfTeamsInt = 4;
+        } else if (view == mFiveTeamButton) {
+            numberOfTeamsInt = 5;
+        } else if (view == mSixTeamButton) {
+            numberOfTeamsInt = 6;
         }
     }
 
